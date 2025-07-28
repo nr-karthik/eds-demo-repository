4ed7b2468e2cdfe37b2c4180b39065ace8ac233a
Karthik Nr <129750991+nr-karthik@users.noreply.github.com>
2025-07-28 16:17:14 +0530

    add charts to section

--- blocks/charts/charts.js
+++ blocks/charts/charts.js
@@ -1,4 +1,5 @@
 export default function decorate(block) {
+	block.textContent = 'Charts block is not implemented yet.';
 	console.log('Charts block is not implemented yet.');
 	console.log(block);
 }
