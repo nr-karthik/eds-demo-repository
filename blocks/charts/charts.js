16b17e9ee2dbd971ca90881271b91360cabf629f
Karthik Nr <129750991+nr-karthik@users.noreply.github.com>
2025-07-29 18:29:17 +0530

    delete chart  height and width

--- blocks/charts/charts.js
+++ blocks/charts/charts.js
@@ -23,8 +23,8 @@ export default async function decorate(block) {
   await waitForChartJS();
   const canvas = document.createElement('canvas');
   canvas.id = 'myChart';
-  canvas.width = 800;
-  canvas.height = 400;
+  // canvas.width = 800;
+  // canvas.height = 400;
   const chartJSON = await getChartJSON();
   block.textContent = JSON.stringify(chartJSON);
   block.appendChild(canvas);
