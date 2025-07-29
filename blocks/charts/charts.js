da14114298610fcdd227f34712522f753226a04a
Karthik Nr <129750991+nr-karthik@users.noreply.github.com>
2025-07-29 16:50:28 +0530

    charts json

--- blocks/charts/charts.js
+++ blocks/charts/charts.js
@@ -1,5 +1,14 @@
-export default function decorate(block) {
-	block.textContent = 'Charts block is not implemented yet.';
+// import Chart from 'chart.js/auto';
+
+const API_URL = 'https://mocki.io/v1/257fd7fc-f7af-4dbd-8396-27923afbd0aa';
+
+async function getChartJSON() {
+	return fetch(API_URL).then((response) => response.json());
+}
+
+export default async function decorate(block) {
+	const chartJSON = await getChartJSON();
+	block.textContent = JSON.stringify(chartJSON);
 	console.log('Charts block is not implemented yet.');
 	console.log(block);
 }
